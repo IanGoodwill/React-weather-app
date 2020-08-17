d906761b72d92297a17e1442b3bfbed9ca3d1ab4
Ian <iangoodwill2@gmail.com>
2020-08-17 15:39:15 -0600

    Added more fields to Conditions.js

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -9,7 +9,11 @@ const conditions = (props) => {
            {props.responseObj.cod === 200 ?
                <div>
                    <p><strong>{props.responseObj.name}</strong></p>
-                   <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
+                   <p>It is currently <strong>{Math.round(props.responseObj.main.temp)}</strong> degrees out with {props.responseObj.weather[0].description}.</p>
+                   <p> The current temperature feels like <strong>{props.responseObj.main.feels_like}</strong> degrees. </p>
+                   <p> The pressure is <strong>{props.responseObj.main.pressure}</strong> and the humidity is <strong>{props.responseObj.main.humidity}</strong> </p>
+                   <p> The wind speed is <strong>{props.responseObj.wind.speed}</strong> km/h. </p>
+                   <p> Sun rise is at {props.responseObj.sys.sunrise} and sun set is at {props.responseObj.sys.sunset}. </p>
                </div>
            : null
            }
