94213969eb8837156b0ca7e2ff224320c633a3d6
Ian <iangoodwill2@gmail.com>
2020-08-17 15:07:23 -0600

    connected Open Weather Map API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Forecast from "./components/Forecast/Forecast";
+import Logo from './components/Logo/Logo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
-      </header>
-
-      <main>
-        {/* add weather fetching component */}
-      </main>
-      <footer>
-        Page created by Ian Goodwill
-      </footer>
-
-    </div>
-  );
+ return (
+   <div className="App">
+     <header className="App-header">
+        <Logo />
+       <h1>React Weather App</h1>
+     </header>
+     <main>
+       <Forecast />
+     </main>
+     <footer>
+       Page created by Ian Goodwill
+     </footer>
+   </div>
+ );
 }
-
 export default App;
